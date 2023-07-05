df6ad0f806365c43e9afd81ec8c205c5af3968dd
ebarnabas <macskabal2000@gmail.com>
2023-07-05 11:47:42 +0200

    Game state handler class added

--- src/gameState.ts
+++ src/gameState.ts
@@ -1,9 +1,48 @@
-interface Player{
-    x: number,
-    y: number,
-    width: number,
-    height: number
+import { HealthComponent } from './entity/Components/healthComponent'
+import { PositionComponent } from './entity/Components/positionComponent'
+import { SpriteComponent } from './entity/Components/spriteComponent'
+import { Entity } from './entity/entity'
+
+type EntityDictionary = { [key: string]: { [key: string]: Entity }}
+type EntityDTO = {
+    health: number,
+    position: { x: number, y: number },
+    sprite: string
 }
 
-export const players: { [key: string]: Player} = {
+type EntityDTODictionary = { [key: string]: EntityDTO[]}
+
+export class GameState{
+    public entities: EntityDictionary
+
+    constructor(){
+        this.entities = {}
+        this.entities['players'] = {}
+    }
+
+    public convertToEntityDTOArray(): EntityDTODictionary{
+        const entityDTODictionary: EntityDTODictionary = {}
+        for (const key in this.entities) {
+            entityDTODictionary[key] = []
+            for (const entityKey in this.entities[key]) {
+                const entityDTO = this.convertEntityToDTO(this.entities[key][entityKey])
+                entityDTODictionary[key].push(entityDTO)
+            }
+        }
+
+        return entityDTODictionary
+    }
+
+    private convertEntityToDTO(entity: Entity): EntityDTO{
+        const healthComponent = entity.getComponent('health') as HealthComponent
+        const positionComponent = entity.getComponent('position') as PositionComponent
+        const spriteComponent = entity.getComponent('sprite') as SpriteComponent
+        const entityDTO: EntityDTO = {
+            health: healthComponent.health,
+            position: { x: positionComponent.position.x, y: positionComponent.position.y },
+            sprite: spriteComponent.spritePath
+        }
+
+        return entityDTO
+    }
 }
