30d224e9936c2212c700ff81642452f8a01ec193
ebarnabas <macskabal2000@gmail.com>
2023-07-12 17:11:09 +0200

    Temporary speed variable added

--- src/entity/Components/positionComponent.ts
+++ src/entity/Components/positionComponent.ts
@@ -7,8 +7,10 @@ type Vec2 = {
 
 export class PositionComponent extends Component {
   public position: Vec2
+  public speed: number
   constructor(public x: number, public y: number) {
     super()
     this.position = {x, y}
+    this.speed = 0
   }
   }
