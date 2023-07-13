ccd6cecf50014a31dd0afc0a856f9585cfc325e5
ebarnabas <macskabal2000@gmail.com>
2023-07-13 17:01:48 +0200

    Using vector library for position

--- src/entity/Components/positionComponent.ts
+++ src/entity/Components/positionComponent.ts
@@ -1,17 +1,12 @@
-import { Component } from './component'
-
-type Vec2 = {
-  x: number,
-  y: number
-}
 import { Component } from './component.js'
+import { Vec2 } from '@thi.ng/vectors'
 
 export class PositionComponent extends Component {
   public position: Vec2
   public speed: number
   constructor(public x: number, public y: number) {
     super()
-    this.position = {x, y}
+    this.position = new Vec2([x, y])
     this.speed = 0
   }
   }
