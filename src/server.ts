d0d7f35a2e7d8a931b3fea230b9f6fbd1105c527
ebarnabas <macskabal2000@gmail.com>
2023-07-24 11:09:14 +0200

    Updating to https and updated cors

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createServer } from 'http';
+import { createServer } from 'https';
 import { Server, Socket } from 'socket.io';
 import cors from 'cors'
 import { GameState } from './gameState.js';
@@ -18,14 +18,14 @@ const server = createServer(app);
 const connectedSockets: string[] = []
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:5173'
+        origin: ['http://localhost:5173', 'https://zefir.iedre.dev/']
     },
     allowEIO3: true,
     pingTimeout: 5000
 });
 
 app.use(cors({
-    origin: 'http://localhost:5173/'
+    origin: ['http://localhost:5173', 'https://zefir.iedre.dev/']
 }))
 
 io.on('connection', (socket: Socket) => {
