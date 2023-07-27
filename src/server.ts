a08a1a7f58253aeae9e891f15ce88d36386d6dab
ebarnabas <macskabal2000@gmail.com>
2023-07-27 16:58:28 +0200

    Formatting and introducing systems

--- src/server.ts
+++ src/server.ts
@@ -1,157 +1,122 @@
-import express from 'express';
-import { createServer } from 'https';
+import express, { Request, Response } from 'express'
+import { Express } from 'express'
 import http from 'http'
-import https from 'https'
-import { Server, Socket } from 'socket.io';
+import { Server, Socket } from 'socket.io'
 import cors from 'cors'
-import { GameState } from './gameState.js';
-import { Entity } from './entity/entity.js';
+import { GameState } from './gameState.js'
+import { Entity } from './entity/entity.js'
 import { HealthComponent } from './entity/Components/healthComponent.js'
 import { PositionComponent } from './entity/Components/positionComponent.js'
-import { SpriteComponent } from './entity/Components/spriteComponent.js'
-import { PlayerCommandManager } from './playerCommandManager.js';
-import { findPlayer } from './utils/taghelpers.js';
-import { MovementComponent } from './entity/Components/movementComponent.js';
+import { RequirementCallback, SpriteComponent } from './entity/Components/spriteComponent.js'
+import { PlayerCommandManager } from './playerCommandManager.js'
+import { MovementComponent } from './entity/Components/movementComponent.js'
+import { ActionQueueComponent } from './entity/Components/actionQueueComponent.js'
+import { InputSystem } from './systems/inputSystem.js'
+import { ActionExecutionSystem } from './systems/actionExecutionSystem.js'
+import { StateComponent } from './entity/Components/stateComponent.js'
+import { AnimationStateSystem } from './systems/animationStateSystem.js'
+import { MovementSystem } from './systems/movementSystem.js'
 
 const startDate = Date.now()
 export const gameState = new GameState()
 const playerCommandManager = new PlayerCommandManager(gameState)
+const inputSystem = new InputSystem(gameState)
+const actionExecutionSystem = new ActionExecutionSystem(gameState)
+const animationStateSystem = new AnimationStateSystem(gameState)
+const movementSystem = new MovementSystem(gameState)
 
-const app: any = express();
+const app: Express = express()
 const httpServer = http.createServer(app)
-const httpsServer = https.createServer(app)
 const connectedSockets: string[] = []
 const io = new Server(httpServer, {
-    cors: {
-        origin: ['http://localhost:5173', 'https://zefir.iedre.dev/', 'https://iedre.dev/']
-    },
-    allowEIO3: true,
-    pingTimeout: 5000
-});
+        cors: {
+                origin: ['http://localhost:5173', 'https://zefir.iedre.dev/', 'https://iedre.dev/']
+        },
+        allowEIO3: true,
+        pingTimeout: 5000
+})
 
-app.use(cors({
-    origin: ['http://localhost:5173', 'https://zefir.iedre.dev/', 'https://iedre.dev/']
-}))
+app.use(
+        cors({
+                origin: ['http://localhost:5173', 'https://zefir.iedre.dev/', 'https://iedre.dev/']
+        })
+)
 
 io.on('connection', (socket: Socket) => {
-  console.log('Player connected: '+ socket.id);
-  // Handle socket events and logic here
+        console.log('Player connected: ' + socket.id)
+        // Handle socket events and logic here
 
-  socket.on('message', (message: string) => {
-    //console.log('Message received: ' + message)
-    socket.broadcast.emit('response', findPlayer(socket.id, gameState.entities)?.name + ": "+ message)
-  })
+        socket.on('message', (message: string) => {
+                //console.log('Message received: ' + message)
+                socket.broadcast.emit('response', gameState.findPlayer(socket.id)?.name + ': ' + message)
+        })
 
-  socket.on('newPlayer', (name: string) => {
-    const player = new Entity(name)
-    console.log('Player '+name+' created')
-    player.addComponent('health', new HealthComponent(100))
-    player.addComponent('position', new PositionComponent(Math.floor(Math.random() * (200 - 50 + 1)) + 50, Math.floor(Math.random() * (200 - 50 + 1)) + 50))
-    player.addComponent('sprite', new SpriteComponent('player.png', true, 'idle'))
-    player.addComponent('movement', new MovementComponent())
-    const spriteComponent = player.getComponent('sprite') as SpriteComponent
-    if(spriteComponent){
-      const movementComponent = player.getComponent('movement') as MovementComponent
-      if(movementComponent){
-        spriteComponent.addState({
-          name: 'idle',
-          requirement: (movementComponent: MovementComponent) => {
-            if(movementComponent.movementVector.x == 0 && movementComponent.movementVector.y == 0){
-              return true
-            }
-            return false
-          },
-          params: [movementComponent]
+        socket.on('newPlayer', (name: string) => {
+                const player = new Entity(name)
+                console.log('Player ' + name + ' created')
+                player.addComponent('health', new HealthComponent(100))
+                player.addComponent(
+                        'position',
+                        new PositionComponent(
+                                Math.floor(Math.random() * (200 - 50 + 1)) + 50,
+                                Math.floor(Math.random() * (200 - 50 + 1)) + 50
+                        )
+                )
+                player.addComponent('sprite', new SpriteComponent('player.png', true, 'idle'))
+                player.addComponent('movement', new MovementComponent())
+                player.addComponent('actionQueue', new ActionQueueComponent())
+                player.addComponent('state', new StateComponent())
+                player.addTag('controlledby', socket.id)
+                player.initComponents()
+                gameState.entities.push(player)
+                connectedSockets.push(socket.id)
+                socket.emit('playerCreated', player.name)
+                socket.broadcast.emit('chat-playerJoined', player.name)
         })
-        spriteComponent.addState({
-          name: 'runRight',
-          requirement: (movementComponent: MovementComponent) => {
-            if(movementComponent.movementVector.x > 0){
-              return true
-            }
-            return false
-          },
-          params: [movementComponent]
-        })
-        spriteComponent.addState({
-          name: 'runLeft',
-          requirement: (movementComponent: MovementComponent) => {
-            if(movementComponent.movementVector.x < 0){
-              return true
-            }
-            return false
-          },
-          params: [movementComponent]
-        })
-        spriteComponent.addState({
-          name: 'runUp',
-          requirement: (movementComponent: MovementComponent) => {
-            if(movementComponent.movementVector.y < 0){
-              return true
-            }
-            return false
-          },
-          params: [movementComponent]
-        })
-        spriteComponent.addState({
-          name: 'runDown',
-          requirement: (movementComponent: MovementComponent) => {
-            if(movementComponent.movementVector.y > 0){
-              return true
-            }
-            return false
-          },
-          params: [movementComponent]
-        })
-        
-      }
-    }
-    player.addTag('controlledby', socket.id)
-    player.initComponents()
-    gameState.entities.push(player)
-    connectedSockets.push(socket.id)
-    socket.emit('playerCreated', player.name)
-    socket.broadcast.emit('chat-playerJoined', player.name)
-    socket.on('playerCommand', (commands: any) => {
-      playerCommandManager.setCommands(commands, socket.id)
-    })
-  })
 
-  socket.on('disconnect', () => {
-    const playerToRemove = findPlayer(socket.id, gameState.entities)
-    if(playerToRemove){
-      gameState.removeEntity(playerToRemove)
-    }
-    console.log("Player disconnected: "+socket.id)
-    const index = connectedSockets.indexOf(socket.id)
-    if(index !== -1){
-      connectedSockets.splice(index, 1)
-    }
-  })
-});
+        socket.on('playerCommand', (commands: string[]) => {
+                //playerCommandManager.setCommands(commands, socket.id)
+                inputSystem.setInputs(commands, socket.id)
+        })
+
+        socket.on('disconnect', () => {
+                const playerToRemove = gameState.findPlayer(socket.id)
+                if (playerToRemove) {
+                        gameState.removeEntity(playerToRemove)
+                }
+                console.log('Player disconnected: ' + socket.id)
+                const index = connectedSockets.indexOf(socket.id)
+                if (index !== -1) {
+                        connectedSockets.splice(index, 1)
+                }
+        })
+})
 
 setInterval(() => {
-  //console.time('tick')
-  playerCommandManager.ExecuteCommands()
-  runEntityLogic()
-  io.sockets.emit('state', gameState.convertToEntityDTOArray())
-  //console.timeEnd('tick')
+        //console.time('tick')
+        //playerCommandManager.ExecuteCommands()
+        actionExecutionSystem.update()
+        movementSystem.update()
+        animationStateSystem.update()
+        runEntityLogic()
+        io.sockets.emit('state', gameState.convertToEntityDTOArray())
+        //console.timeEnd('tick')
 }, 1000 / 60)
 
-app.get('/', (req: any, res: any) => {
-  res.send('Hello, this is your Express.js server! Running since: '+startDate);
-});
+app.get('/', (req: Request, res: Response) => {
+        res.send('Hello, this is your Express.js server! Running since: ' + startDate)
+})
 
-app.get('/health', (req: any, res: any) => {
-  res.sendStatus(200)
-});
+app.get('/health', (req: Request, res: Response) => {
+        res.sendStatus(200)
+})
 
 httpServer.listen(3000, () => {
-  console.log('Socket server listening on port 3000');
-});
+        console.log('Socket server listening on port 3000')
+})
 
-function runEntityLogic(){
-  for (const entity of gameState.entities) {
-    entity.updateComponents()
-  }
+function runEntityLogic() {
+        for (const entity of gameState.entities) {
+                entity.updateComponents()
+        }
 }
