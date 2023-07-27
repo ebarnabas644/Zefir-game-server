7adc0f2b8190c6d94cd766c460e451f2c0b203a7
ebarnabas <macskabal2000@gmail.com>
2023-07-27 16:55:40 +0200

    Reducing functionality into just holding state

--- src/entity/Components/movementComponent.ts
+++ src/entity/Components/movementComponent.ts
@@ -1,38 +1,25 @@
-import { IScript } from '../Interfaces/IScript.js'
 import { Component } from './component.js'
 import { Vec2 } from '@thi.ng/vectors'
-import { PositionComponent } from './positionComponent.js'
-import * as v from "@thi.ng/vectors";
+import * as v from '@thi.ng/vectors'
 
-export class MovementComponent extends Component implements IScript{
-    public movementVector: Vec2
-    private speed: number
-    private positionComponent?: PositionComponent
+export class MovementComponent extends Component {
+        public movementVector: Vec2
+        public speed: number
 
-    constructor(){
-        super()
-        this.speed = 7
-        this.movementVector = new Vec2([0,0])
-    }
-
-    setMovementVector(x: number, y: number){
-        this.movementVector.x = x
-        this.movementVector.y = y
-        v.normalize(null, this.movementVector, this.speed)
-    }
-
-    stopMovement(){
-        this.movementVector.x = 0
-        this.movementVector.y = 0
-    }
-
-    public start(): void {
-        this.positionComponent = this.entity?.getComponent('position') as PositionComponent
-    }
-
-    public update(): void {
-        if(this.positionComponent){
-            v.add(null, this.positionComponent.position, this.movementVector)
+        constructor() {
+                super()
+                this.speed = 7
+                this.movementVector = new Vec2([0, 0])
         }
-    }
+
+        setMovementVector(x: number, y: number) {
+                this.movementVector.x = x
+                this.movementVector.y = y
+                v.normalize(null, this.movementVector, this.speed)
+        }
+
+        stopMovement() {
+                this.movementVector.x = 0
+                this.movementVector.y = 0
+        }
 }
--- src/entity/Components/spriteComponent.ts
+++ src/entity/Components/spriteComponent.ts
@@ -1,34 +1,37 @@
-import { IScript } from '../Interfaces/IScript.js'
 import { Component } from './component.js'
 
+export type RequirementCallback = (...params: unknown[]) => boolean
+
 export type State<T> = {
-  name: string,
-  requirement: (...params: any[]) => boolean,
-  params: T[]
+        name: string
+        requirement: RequirementCallback
+        params: T[]
 }
 
-export class SpriteComponent extends Component implements IScript {
-  private states: State<any>[]
-  constructor(public spritePath: string, public animated: boolean = false, public state: string = "default"){
-    super()
-    this.states = []
-  }
+export class SpriteComponent extends Component {
+        private states: State<unknown>[]
+        constructor(
+                public spritePath: string,
+                public animated: boolean = false,
+                public state: string = 'default'
+        ) {
+                super()
+                this.states = []
+        }
 
-  public addState(state: State<any>){
-    this.states.push(state)
-  }
+        public addState(state: State<unknown>) {
+                this.states.push(state)
+        }
 
-  public start(): void {
-        
-  }
+        public start(): void {}
 
-  public update(){
-    for (const state of this.states) {
-      const isRequirementMet = state.requirement(...state.params)
-      if(isRequirementMet){
-        this.state = state.name
-        break
-      }
-    }
-  }
+        public update() {
+                for (const state of this.states) {
+                        const isRequirementMet = state.requirement(...state.params)
+                        if (isRequirementMet) {
+                                this.state = state.name
+                                break
+                        }
+                }
+        }
 }
