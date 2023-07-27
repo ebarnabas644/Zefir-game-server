112376bc71712960ef035b87214b5ef39490a5b3
ebarnabas <macskabal2000@gmail.com>
2023-07-27 16:57:47 +0200

    Formatting and moving taghelpers into game state

--- src/gameState.ts
+++ src/gameState.ts
@@ -5,56 +5,67 @@ import { Entity } from './entity/entity.js'
 
 export type EntityDictionary = Entity[]
 export type EntityDTO = {
-    id: number,
-    name: string,
-    health: number,
-    position: { x: number, y: number },
-    sprite: string,
-    state: string,
-    tags: {[key: string]: any}
+        id: number
+        name: string
+        health: number
+        position: { x: number; y: number }
+        sprite: string
+        state: string
+        tags: { [key: string]: any }
 }
 
 type EntityDTODictionary = EntityDTO[]
 
-export class GameState{
-    public entities: EntityDictionary
+export class GameState {
+        public entities: EntityDictionary
 
-    constructor(){
-        this.entities = []
-    }
-
-    public convertToEntityDTOArray(): EntityDTODictionary{
-        const entityDTODictionary: EntityDTODictionary = []
-        for (let index = 0; index < this.entities.length; index++) {
-            const entityDTO = this.convertEntityToDTO(this.entities[index])
-            entityDTODictionary.push(entityDTO)
+        constructor() {
+                this.entities = []
         }
 
-        return entityDTODictionary
-    }
+        public convertToEntityDTOArray(): EntityDTODictionary {
+                const entityDTODictionary: EntityDTODictionary = []
+                for (let index = 0; index < this.entities.length; index++) {
+                        const entityDTO = this.convertEntityToDTO(this.entities[index])
+                        entityDTODictionary.push(entityDTO)
+                }
 
-    convertEntityToDTO(entity: Entity): EntityDTO{
-        const healthComponent = entity.getComponent('health') as HealthComponent
-        const positionComponent = entity.getComponent('position') as PositionComponent
-        const spriteComponent = entity.getComponent('sprite') as SpriteComponent
-        const entityDTO: EntityDTO = {
-            id: entity.id,
-            name: entity.name,
-            health: healthComponent.health,
-            position: { x: positionComponent.position.x, y: positionComponent.position.y },
-            sprite: spriteComponent.spritePath,
-            state: spriteComponent.state,
-            tags: {}
+                return entityDTODictionary
         }
 
-        for (const key in entity.tags) {
-            entityDTO.tags[key] = entity.tags[key]
+        convertEntityToDTO(entity: Entity): EntityDTO {
+                const healthComponent = entity.getComponent('health') as HealthComponent
+                const positionComponent = entity.getComponent('position') as PositionComponent
+                const spriteComponent = entity.getComponent('sprite') as SpriteComponent
+                const entityDTO: EntityDTO = {
+                        id: entity.id,
+                        name: entity.name,
+                        health: healthComponent.health,
+                        position: {
+                                x: positionComponent.position.x,
+                                y: positionComponent.position.y
+                        },
+                        sprite: spriteComponent.spritePath,
+                        state: spriteComponent.state,
+                        tags: {}
+                }
+
+                for (const key in entity.tags) {
+                        entityDTO.tags[key] = entity.tags[key]
+                }
+
+                return entityDTO
         }
 
-        return entityDTO
-    }
+        removeEntity(entity: Entity) {
+                this.entities = this.entities.filter((element) => element.id !== entity.id)
+        }
 
-    removeEntity(entity: Entity){
-        this.entities = this.entities.filter(element => element.id !== entity.id)
-    }
+        findPlayer(socketId: string) {
+                return this.entities.find((entity) => entity.findTag('controlledby') == socketId)
+        }
+
+        findPlayers() {
+                return this.entities.filter((entity) => entity.findTag('controlledby'))
+        }
 }
