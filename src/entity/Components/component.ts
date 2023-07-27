df6d7c3586b1ae16f5e5205119e7c2880f981b2f
ebarnabas <macskabal2000@gmail.com>
2023-07-27 16:55:01 +0200

    Formatting

--- src/entity/Components/component.ts
+++ src/entity/Components/component.ts
@@ -1,13 +1,13 @@
 import { Entity } from '../entity.js'
 
-export abstract class Component{
-    protected entity?: Entity
-  
-    public onAttach(entity: Entity){
-      this.entity = entity
-    }
-  
-    public onDetach(entity: Entity){
-      this.entity = undefined
-    }
-  }
+export abstract class Component {
+        public entity?: Entity
+
+        public onAttach(entity: Entity) {
+                this.entity = entity
+        }
+
+        public onDetach(entity: Entity) {
+                this.entity = undefined
+        }
+}
--- src/entity/Components/positionComponent.ts
+++ src/entity/Components/positionComponent.ts
@@ -2,11 +2,14 @@ import { Component } from './component.js'
 import { Vec2 } from '@thi.ng/vectors'
 
 export class PositionComponent extends Component {
-  public position: Vec2
-  public speed: number
-  constructor(public x: number, public y: number) {
-    super()
-    this.position = new Vec2([x, y])
-    this.speed = 0
-  }
-  }
+        public position: Vec2
+        public speed: number
+        constructor(
+                public x: number,
+                public y: number
+        ) {
+                super()
+                this.position = new Vec2([x, y])
+                this.speed = 0
+        }
+}
