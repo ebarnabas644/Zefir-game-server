b0ab092c4759c09e04aaf9f8681ae68dae7d7048
ebarnabas <macskabal2000@gmail.com>
2023-07-27 16:59:45 +0200

    Base world system class added

---
+++ src/systems/worldSystem.ts
@@ -0,0 +1,12 @@
+import { ISystem } from './Interfaces/ISystem.js'
+
+export class WorldSystem implements ISystem {
+        constructor() {}
+
+        start(): void {
+                throw new Error('Method not implemented.')
+        }
+        update(): void {
+                throw new Error('Method not implemented.')
+        }
+}
