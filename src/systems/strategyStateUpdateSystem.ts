cb300d412feef645adf3fcee0be26250e2b20ea1
ebarnabas <macskabal2000@gmail.com>
2023-08-01 16:47:55 +0200

    Strategy state update system added

---
+++ src/systems/strategyStateUpdateSystem.ts
@@ -0,0 +1,40 @@
+import { PositionComponent } from '../entity/Components/positionComponent.js'
+import { StrategyComponent } from '../entity/Components/strategyComponent.js'
+import { GameState } from '../gameState.js'
+import { ISystem } from './Interfaces/ISystem.js'
+import * as v from '@thi.ng/vectors'
+
+export class StrategyStateUpdateSystem implements ISystem {
+        state: GameState
+        constructor(state: GameState) {
+                this.state = state
+        }
+        start(): void {
+                throw new Error('Method not implemented.')
+        }
+        update(): void {
+                const strategyComponents = this.state.entities
+                        .map((x) => x.getComponent('strategy') as StrategyComponent)
+                        .filter((x) => x !== undefined)
+                strategyComponents.forEach((x) => this.calculateStrategyState(x))
+        }
+
+        calculateStrategyState(strategyComponent: StrategyComponent) {
+                const entity = strategyComponent.entity
+                if (!entity) return
+                const mobPosition = entity.getComponent('position') as PositionComponent
+                if (!mobPosition) return
+                const mobTargets = this.state.entities.filter((x) => {
+                        const tag = x.findTag('targetableByMob')
+                        return tag !== undefined && tag == true
+                })
+                mobTargets.forEach((entity) => {
+                        const targetPos = entity.getComponent('position') as PositionComponent
+                        if (!targetPos) return
+                        const distance = v.dist2(targetPos.position, mobPosition.position)
+                        if (distance <= 100) {
+                                strategyComponent.strategy = 'combat'
+                        }
+                })
+        }
+}
