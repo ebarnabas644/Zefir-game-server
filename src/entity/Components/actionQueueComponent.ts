b114a2def6ce23a123dd6f41a68487fe6179630f
ebarnabas <macskabal2000@gmail.com>
2023-08-02 19:57:31 +0200

    Helper methods

--- src/entity/Components/actionQueueComponent.ts
+++ src/entity/Components/actionQueueComponent.ts
@@ -2,6 +2,7 @@ import { Component } from './component.js'
 
 export type Action = {
         name: string
+        duration: number
         data?: { [key: string]: unknown }
 }
 
@@ -18,15 +19,24 @@ export class ActionQueueComponent extends Component {
                 return result !== undefined
         }
 
+        public hasActionByName(name: string): boolean {
+                const result = this.actions.find((x) => x.name == name)
+                return result !== undefined
+        }
+
         public addActionToQueue(actionToAdd: Action) {
-                let exists = false
-                for (const action of this.actions) {
-                        if (action.name == actionToAdd.name) {
-                                exists = true
-                                break
-                        }
-                }
-                if (exists) return
+                const result = this.actions.find((x) => x.name == actionToAdd.name)
+                if (result) return
                 this.actions.push(actionToAdd)
+        }
+
+        public removeAction(action: Action) {
+                const index = this.actions.indexOf(action)
+                if (index == -1) return
+                this.actions.splice(index, 1)
+        }
+
+        public clearQueue() {
+                this.actions = []
         }
 }
