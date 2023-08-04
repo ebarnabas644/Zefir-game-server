202ea64c2ed5847a1042f234034d603c1ec16997
ebarnabas <macskabal2000@gmail.com>
2023-08-04 16:55:30 +0200

    DI container configuration moved and added

---
+++ src/inversify.config.ts
@@ -0,0 +1,16 @@
+import { Container } from 'inversify'
+import { TYPES } from './inversify.types.js'
+import { ISystem } from './systems/Interfaces/ISystem.js'
+import { AnimationStateSystem } from './systems/animationStateSystem.js'
+import { IGameState } from './gameState/IGameState.js'
+import { GameState } from './gameState/gameState.js'
+import { buildProviderModule } from 'inversify-binding-decorators'
+
+// TODO: generate side effect imports here with plop library
+const container = new Container()
+container.bind<ISystem>(TYPES.ISystem).to(AnimationStateSystem)
+
+container.bind<IGameState>(TYPES.IGameState).to(GameState).inSingletonScope()
+container.load(buildProviderModule())
+console.log(container)
+export { container }
